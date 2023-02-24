200ed73c05073c619c33c2f2defbc6015deb9727
luis <luisrym@gmail.com>
2023-02-24 14:45:51 +0100

    resolve hot spot security in app

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import apiRouter from './api/api-router.js';
 
 const app = express();
-
+app.disable('x-powered-by');
 app.get('/', (req, res) => {
   res.json('Server is up!!');
 });
+
 app.use(express.json());
 app.use('/api/v1', apiRouter);
 
