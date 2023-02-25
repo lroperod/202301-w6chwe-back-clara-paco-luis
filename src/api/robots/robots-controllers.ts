b56603d6dd91561019b18688e4958ef14e1f76ca
claramarsango <claraxmarsango@gmail.com>
2023-02-25 17:16:12 +0100

    add post method to create robots

--- src/api/robots/robots-controllers.ts
+++ src/api/robots/robots-controllers.ts
@@ -1,10 +1,25 @@
 import { RequestHandler } from 'express';
-import { RobotModel } from './robot-schema';
+import crypto from 'node:crypto';
+import { RobotModel } from './robot-schema.js';
 
 export const getRobotsControllers: RequestHandler = async (_req, res) => {
   try {
     const foundRobots = await RobotModel.find({});
     res.json(foundRobots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const createRobotController: RequestHandler = async (req, res) => {
+  const id = crypto.randomUUID();
+  const robot: typeof RobotModel = {
+    id,
+    ...req.body,
+  };
+  try {
+    await RobotModel.create(robot);
+    res.status(201).json(robot);
   } catch (error) {
     res.status(500).json(error);
   }
