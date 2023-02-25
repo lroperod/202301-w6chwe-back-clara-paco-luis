9f4e87d5b40dd5d76c699f81f8fe882744a33592
luis <luisrym@gmail.com>
2023-02-25 18:33:57 +0100

    create function to request delete

--- src/api/robots/robots-controllers.ts
+++ src/api/robots/robots-controllers.ts
@@ -9,3 +9,18 @@ export const getRobotsControllers: RequestHandler = async (_req, res) => {
     res.status(500).json(error);
   }
 };
+
+export const deleteRobotByIdController: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dbRes = await RobotModel.deleteOne({ id });
+    if (dbRes.deletedCount === 0) {
+      res.sendStatus(404);
+    } else {
+      res.json(id);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
