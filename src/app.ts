1879002b3d2067c71e5800bce476c6bebc67912f
luis <luisrym@gmail.com>
2023-02-26 17:01:34 +0100

    add dependecies cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import apiRouter from './api/api-router.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 app.disable('x-powered-by');
 app.get('/', (req, res) => {
   res.json('Server is up!!');
